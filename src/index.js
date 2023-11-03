3d7e58bd25cc91ab5ae202d86b02aad9c47549d2
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-11-03 19:05:58 +0900

    fix: index.js 충돌 해결

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// 테마 생성
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3054d3",
+    },
+    secondary: {
+      main: "#8797d3",
+    },
+  },
+  // 추가적인 테마 설정...
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
