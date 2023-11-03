b491f708904525d3dfc84efca2085c404a1f9456
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-11-03 19:37:19 +0900

    fix: 에러 수정

--- src/pages/order/DirectOrder.js
+++ src/pages/order/DirectOrder.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import { Grid } from "@mui/material";
-import CartCheck from "../cart/CartCheck";
-import CartOne from "../cart/CartOne";
+import CartCheck from "../../components/cart/CartCheck";
+import CartOne from "../../components/cart/CartOne";
 
 const Cart = () => {
   const carsData = [
     {
       id: 1,
-      name: '차량 1',
-      maxSpeed: '225km/h',
-      mileage: '10km/l',
-      fuel: '5.7',
-      color: 'Black',
-      image: '/car1.jpg',
-      price: '50,000,000',
-      option: '라이트',
+      name: "차량 1",
+      maxSpeed: "225km/h",
+      mileage: "10km/l",
+      fuel: "5.7",
+      color: "Black",
+      image: "/car1.jpg",
+      price: "50,000,000",
+      option: "라이트",
     },
   ];
 
@@ -24,7 +24,7 @@ const Cart = () => {
     <div style={{ padding: "16px" }}>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-            <CartOne car={cart[0]} setCart={setCart}/>
+          <CartOne car={cart[0]} setCart={setCart} />
         </Grid>
 
         <Grid item xs={6}>
