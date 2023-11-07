ce69b9ae6746a2c0fd02fa25e45a9e4297586a3c
hido <bestdohee@gmail.com>
2023-11-07 14:09:27 +0900

    feat: 사용자 정보 페이지 api 분리

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,3 +3,34 @@ import axios from "axios";
 export const login = (loginInfo) => {
   return axios.post("/api/signin", loginInfo).then((response) => response);
 };
+
+export const signup = (userData) => {
+  return axios.post("/api/signup", userData);
+};
+
+// 사용자 정보 가져오기
+export const getUserInfo = (shortId, accessToken) => {
+  return axios.get(`/api/users/${shortId}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+};
+
+// 사용자 정보 업데이트
+export const updateUserInfo = (shortId, accessToken, updatedUserInfo) => {
+  return axios.put(`/api/users/${shortId}`, updatedUserInfo, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+};
+
+// 사용자 계정 삭제
+export const deleteUserInfo = (shortId, accessToken) => {
+  return axios.delete(`/api/users/${shortId}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+};
--- src/pages/user/Mypage.js
+++ src/pages/user/Mypage.js
@@ -4,12 +4,9 @@ import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DaumPostcode from "react-daum-postcode";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { getUserInfo, updateUserInfo, deleteUserInfo } from "../../lib/api";
 
 const Mypage = () => {
-  const navigate = useNavigate();
-
   const [user, setUser] = useState({
     userName: "",
     email: "",
@@ -21,102 +18,43 @@ const Mypage = () => {
   const shortId = localStorage.getItem("shortId");
   const accessToken = localStorage.getItem("accessToken");
 
+  // useEffect에서 사용자 정보를 가져옵니다.
   useEffect(() => {
-    if (accessToken) {
-      // 사용자 정보를 서버에서 가져옵니다.
-      axios
-        .get(`api/users/${shortId}`, {
-          // shortId를 URL에 포함
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
+    if (accessToken && shortId) {
+      getUserInfo(shortId, accessToken)
         .then((response) => {
-          if (response.status === 200) {
-            setUser(response.data.user);
-            console.log(user);
-          }
+          setUser(response.data.user);
         })
         .catch((error) => {
           console.error("사용자 정보 가져오기 실패:", error);
         });
     }
-  }, []);
+  }, [accessToken, shortId]);
 
-  const handleAccountUpdate = () => {
-    // 변경된 사용자 정보를 수집
-    const updatedUserInfo = {
-      userName: user.name,
-      email: user.email,
-      age: user.age,
-      phone: user.phoneNumber,
-      address: user.address,
-    };
-
-    const apiUrl = `api/users/${shortId}`; // shortId를 어떻게 얻는지에 따라 동적으로 생성
-
-    // API 요청 헤더 설정
-
-    const headers = {
-      Authorization: `Bearer ${accessToken}`,
-    };
-
-    // API 요청 본문 준비
-    const requestBody = updatedUserInfo;
-
-    // API 요청 보내기
-    axios
-      .put(apiUrl, requestBody, { headers })
-      .then((response) => {
-        if (response.status === 200) {
-          // 계정 정보 변경에 성공하면 성공 메시지를 표시하거나 다른 조치를 취할 수 있습니다.
-          alert("계정 정보가 성공적으로 변경되었습니다.");
-        }
-      })
-      .catch((error) => {
-        console.error("API 호출 실패:", error);
-        // 에러 처리를 수행하거나 에러 메시지를 표시할 수 있습니다.
+  // 사용자 정보 업데이트를 위한 핸들러
+  const handleAccountUpdate = async () => {
+    try {
+      await updateUserInfo(shortId, accessToken, {
+        userName: user.userName,
+        email: user.email,
+        age: user.age,
+        phone: user.phone,
+        address: user.address,
       });
+      alert("계정 정보가 성공적으로 변경되었습니다.");
+    } catch (error) {
+      console.error("API 호출 실패:", error);
+    }
   };
 
-  const handleLogout = () => {
-    // 로그아웃 로직을 구현합니다.
-    // 이 예제에서는 localStorage에서 토큰을 제거하여 로그아웃하는 것으로 가정합니다.
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("shortId");
-
-    // 다음으로, 사용자를 로그인 페이지 또는 다른 적절한 페이지로 리디렉션할 수 있습니다.
-  };
-
-  const handleAccountDelete = () => {
-    // 사용자 정보를 서버에서 삭제합니다.
-    axios
-      .delete(`api/users/${shortId}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((response) => {
-        if (response.status === 204) {
-          // 계정 삭제에 성공하면 성공 메시지를 표시하거나 다른 조치를 취할 수 있습니다.
-          alert("계정이 성공적으로 삭제되었습니다.");
-          handleLogout();
-          navigate("/");
-        }
-      })
-      .catch((error) => {
-        if (error.response) {
-          if (error.response.status === 404) {
-            alert("존재하지 않는 계정입니다.");
-          } else if (error.response.status === 400) {
-            alert("토큰이 없습니다. 로그인이 필요합니다.");
-          } else {
-            alert("탈퇴 중에 문제가 발생했습니다.");
-          }
-        } else {
-          alert("탈퇴 중에 문제가 발생했습니다.");
-        }
-      });
+  // 사용자 계정 삭제를 위한 핸들러
+  const handleAccountDelete = async () => {
+    try {
+      await deleteUserInfo(shortId, accessToken);
+      alert("계정이 성공적으로 삭제되었습니다.");
+    } catch (error) {
+      console.error("API 호출 실패:", error);
+    }
   };
 
   const daum = window.daum;
@@ -181,7 +119,7 @@ const Mypage = () => {
             margin="normal"
             value={user.userName}
             onChange={handleInputChange}
-            name="name"
+            name="userName"
           />
           <Typography variant="h6" style={{ fontWeight: "bold" }}>
             이메일
