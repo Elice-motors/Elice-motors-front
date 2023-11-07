0b6ae7330f0e3d1c9b86c389386798996a36bc28
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-11-07 16:05:16 +0900

    feat: 로그아웃 및 차량 상세 및 옵션 API 유틸 함수

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,6 +4,21 @@ export const login = (loginInfo) => {
   return axios.post("/api/signin", loginInfo).then((response) => response);
 };
 
+export const logout = () => {
+  const accessToken = localStorage.getItem("accessToken");
+  return axios
+    .post(
+      "/api/signout",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
+    .then((response) => response);
+};
+
 export const getBannerProduct = () => {
   return axios.get("/api/cars/2").then((response) => response);
 };
@@ -18,4 +33,12 @@ export const getCategorySuv = () => {
 
 export const getCategoryElectric = () => {
   return axios.get("/api/category/electric").then((response) => response);
+};
+
+export const getCarDetail = (carId) => {
+  return axios.get(`/api/cars/${carId}`).then((response) => response);
+};
+
+export const getOptions = () => {
+  return axios.get("/api/car-options").then((response) => response);
 };
