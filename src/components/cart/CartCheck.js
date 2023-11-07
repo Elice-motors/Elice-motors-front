08fcf8b3b84ec694450a3b0c5a8918fc22c80aca
hido <bestdohee@gmail.com>
2023-11-07 15:44:58 +0900

    fix: 사용자 정보 조회 api

--- src/components/cart/CartCheck.js
+++ src/components/cart/CartCheck.js
@@ -7,7 +7,7 @@ import Divider from "@mui/material/Divider";
 import { Typography, Dialog, DialogTitle, DialogContent } from "@mui/material";
 import { Link } from "react-router-dom";
 import DaumPostcode from "react-daum-postcode";
-import axios from "axios";
+import { fetchUserInfo, createPayment } from "../../lib/api";
 
 const CartCheck = ({ cart, userId, setUserId }) => {
   const totalAmount = cart.reduce((total, car) => total + car.price, 0);
@@ -26,80 +26,55 @@ const CartCheck = ({ cart, userId, setUserId }) => {
   };
 
   const useAccountAddress = () => {
-    const accountAddressValue = "Account Address Here";
-
-    setUsingAccountAddress(true);
-    setAddress(accountAddressValue);
-  };
-
-  const accessToken = localStorage.getItem("accessToken");
-  const shortId = localStorage.getItem("shortId");
-
-  const fetchUserInfo = async () => {
-    if (!accessToken) {
-      console.log("사용자가 로그인하지 않았습니다.");
-      // 로그인 페이지로 리다이렉트하는 로직을 추가할 수 있습니다.
-      return;
-    }
-
-    try {
-      const response = await axios.get(`/api/users/${shortId}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
-      // 여기에 사용자 정보를 처리하는 로직을 추가하세요.
-      console.log(response.data.user);
-      setUser(response.data.user);
-      setUserId(response.data.user.id);
-      // 예를 들어, 사용자 정보를 state에 저장하거나 다른 작업을 수행할 수 있습니다.
-    } catch (error) {
-      console.error("사용자 정보 가져오기 실패:", error.response || error);
-      // 사용자에게 오류가 발생했음을 알리거나 로그인 페이지로 리다이렉트할 수 있습니다.
+    if (user) {
+      // 'user' 객체에 저장된 계정 주소와 이름을 사용합니다.
+      setUsingAccountAddress(true);
+      setAddress(user.address);
+      setName(user.userName);
+    } else {
+      console.log("사용자 정보가 없습니다.");
+      // 여기에서 로그인 페이지로 리다이렉트하거나 사용자에게 알림을 줄 수 있습니다.
     }
   };
 
-  useEffect(() => {
-    fetchUserInfo();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetchUserInfo(shortId);
+  //     console.log(response);
+  //   };
+  //   fetchData();
+  // }, []);
 
   // POST 요청을 보내는 함수
-  const handlePayment = async () => {
-    // API 요청에 필요한 데이터를 구성
-    const paymentData = {
-      products: cart.map((item) => ({
-        productInfo: {
-          name: item.name,
-          color: item.color,
-          option: item.option,
-          price: item.price,
-        },
-        quantity: item.quantity,
-      })),
-      amountInfo: totalAmount,
-      userId: userId,
-      address: address,
-      status: "주문 완료",
-    };
+  // const handlePayment = () => {
+  //   // API 요청에 필요한 데이터를 구성
+  //   const paymentData = {
+  //     products: cart.map((item) => ({
+  //       productInfo: {
+  //         name: item.name,
+  //         color: item.color,
+  //         option: item.option,
+  //         price: item.price,
+  //       },
+  //       quantity: item.quantity,
+  //     })),
+  //     amountInfo: totalAmount,
+  //     userId: userId,
+  //     address: address,
+  //     status: "주문 완료",
+  //   };
 
-    try {
-      // API에 POST 요청을 보냅니다
-      const response = await axios.post("/api/payment", paymentData, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      // 성공적으로 응답이 온 경우 처리
-      console.log(response.data);
-      // 여기에 성공 시 로직을 추가하세요. 예: 페이지 리다이렉트 등
-    } catch (error) {
-      // 오류 처리
-      console.error("Payment API Error:", error);
-      // 사용자에게 오류가 발생했음을 알리는 로직을 추가하세요.
-    }
-  };
+  //   if (accessToken) {
+  //     createPayment(paymentData, accessToken)
+  //       .then((data) => {
+  //         console.log("결제 성공", data);
+  //         // 여기에 성공 시 로직을 추가하세요. 예: 페이지 리다이렉트 등
+  //       })
+  //       .catch((error) => {
+  //         console.error("Payment API Error:", error);
+  //       });
+  //   }
+  // };
 
   return (
     <>
@@ -158,14 +133,14 @@ const CartCheck = ({ cart, userId, setUserId }) => {
             </DialogContent>
           </Dialog>
           <Link to="/ordersuccess">
-            <Button
+            {/* <Button
               variant="contained"
               color="primary"
               style={{ width: "100%" }}
               onClick={handlePayment} // 결제 함수 연결
             >
               결제하기
-            </Button>
+            </Button> */}
           </Link>
         </CardContent>
       </Card>
--- src/lib/api.js
+++ src/lib/api.js
@@ -3,3 +3,24 @@ import axios from "axios";
 export const login = (loginInfo) => {
   return axios.post("/api/signin", loginInfo).then((response) => response);
 };
+
+export const fetchUserInfo = (shortId) => {
+  const accessToken = localStorage.getItem("accessToken");
+  return axios
+    .get(`/api/users/${shortId}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((response) => response); // 직접 data를 반환합니다.
+};
+
+export const createPayment = (paymentData, accessToken) => {
+  return axios
+    .post("/api/payment", paymentData, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((response) => response.data); // 직접 data를 반환합니다.
+};
