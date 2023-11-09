af5199c1b73806c2568b4b386fad4ae4e51bfedc
hido <bestdohee@gmail.com>
2023-11-10 02:48:34 +0900

    feat: 관리자 상품 조회, 상품 삭제 기능, UI 개선

--- src/pages/admin/admin-item/AdminItem.js
+++ src/pages/admin/admin-item/AdminItem.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { getAllProducts, deleteProduct } from "../../../lib/api";
-import { Typography, Box, Button } from "@mui/material";
+import { Typography, Box, Button, Stack, Modal } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline"; // MUI 아이콘 추가
 import AdminItemAddModal from "./AdminItemAddModal";
 
 const AdminItem = () => {
   const [products, setProducts] = useState([]);
-  const [open, setOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     // 화면 진입 시 전체 상품 조회
@@ -35,39 +36,120 @@ const AdminItem = () => {
   };
 
   const handleAddProduct = () => {
-    setOpen(true);
+    setModalOpen(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const handleCloseModal = () => {
+    setModalOpen(false);
   };
+
+  console.log(products);
 
   return (
     <>
-      <Typography>관리자 상품 관리 페이지</Typography>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        mb={2}
+      >
+        <Typography variant="h5">관리자 상품 관리 페이지</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddCircleOutlineIcon />} // 버튼에 아이콘 추가
+          onClick={handleAddProduct}
+          sx={{ alignSelf: "flex-end" }} // 버튼을 오른쪽 상단에 위치시킴
+        >
+          새로운 상품 등록
+        </Button>
+      </Stack>
       <Typography variant="subtitle1">전체 상품({products.length})</Typography>
-      <Button onClick={handleAddProduct}>새로운 상품 등록</Button>
-      {open && <AdminItemAddModal open={open} handleClose={handleClose} />}
+      <Modal
+        open={modalOpen} // Modal의 열림 상태를 modalOpen 변수로 제어
+        onClose={handleCloseModal} // 모달 밖의 영역 클릭 시 모달 닫기
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <AdminItemAddModal open={modalOpen} handleClose={handleCloseModal} />
+      </Modal>
       {products.map((product) => (
         <Box
           key={product.carId}
-          sx={{ display: "flex", flexDirection: "column" }}
+          sx={{
+            display: "flex",
+            flexDirection: "row", // 방향을 가로로 설정
+            alignItems: "flex-start", // 상품 내용을 상단 정렬
+            width: 300, // 박스 너비 설정
+            backgroundColor: "#fff", // 박스 배경색 설정
+            boxShadow: "0 2px 8px rgba(0,0,0,0.1)", // 박스 그림자 설정
+            borderRadius: "8px", // 박스 둥근 모서리 설정
+            overflow: "hidden", // 내용이 넘칠 경우 숨김
+            mb: 2, // 아래쪽 마진 설정
+            position: "relative", // 상대적 위치 설정
+          }}
         >
           <img
             src={product.img}
             alt={product.carName}
-            style={{ width: "150px", height: "100px" }}
+            style={{
+              width: "150px", // 이미지 너비 설정
+              objectFit: "cover", // 이미지 비율 유지
+              borderRadius: "8px 0 0 8px", // 왼쪽 상단과 하단의 둥근 모서리 설정
+            }}
           />
-          <Typography variant="subtitle1">{product.carName}</Typography>
-          <Typography variant="subtitle2">속도: {product.speed}</Typography>
-          <Typography variant="subtitle2">
-            주행거리: {product.milege}
-          </Typography>
-          <Typography variant="subtitle2">연료: {product.fuel}</Typography>
-          <Typography variant="subtitle2">색상: {product.color}</Typography>
-          <Button onClick={() => handleDeleteProduct(product.carId)}>
-            상품 삭제
-          </Button>
+          <Box
+            sx={{
+              padding: "0 16px", // 패딩 설정
+              textAlign: "left", // 텍스트 왼쪽 정렬
+              display: "flex", // flexbox 레이아웃 사용
+              flexDirection: "column", // 세로 방향으로 항목 정렬
+              justifyContent: "space-between", // 내용을 상하로 균등 분포
+              width: "100%", // 나머지 공간을 모두 차지하도록 설정
+            }}
+          >
+            {/* 상품 정보 */}
+            <Box>
+              <Typography
+                variant="subtitle1"
+                component="h2"
+                sx={{ fontWeight: "bold" }}
+              >
+                {product.carName}
+              </Typography>
+              <Typography variant="subtitle2">
+                속도: {product.speed} km/h
+              </Typography>
+              <Typography variant="subtitle2">
+                주행거리: {product.mileage} km
+              </Typography>
+              <Typography variant="subtitle2">연료: {product.fuel}</Typography>
+              <Typography variant="subtitle2">색상: {product.color}</Typography>
+              <Typography
+                variant="subtitle2"
+                sx={{ color: "red", mt: 1, fontWeight: "bold" }}
+              >
+                결제 금액: {product.carPrice.toLocaleString()}원
+              </Typography>
+            </Box>
+            <Button
+              onClick={() => handleDeleteProduct(product.carId)}
+              size="small"
+              variant="contained"
+              sx={{
+                backgroundColor: "red",
+                color: "white",
+                "&:hover": { backgroundColor: "darkred" },
+                padding: "6px 16px",
+                fontSize: "0.75rem",
+                alignSelf: "flex-start", // 버튼을 박스의 시작 부분에 정렬
+                mt: 1, // 위쪽 마진 설정
+                mb: 1, // 아래쪽 마진 설정
+              }}
+            >
+              상품 삭제
+            </Button>
+          </Box>
         </Box>
       ))}
     </>
--- src/pages/admin/admin-item/AdminItemAddModal.js
+++ src/pages/admin/admin-item/AdminItemAddModal.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
-import { Box, TextField, Button, Select } from "@mui/material";
+import {
+  Box,
+  TextField,
+  Button,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { addProduct } from "../../../lib/api";
-import { nanoid } from "nanoid";
 
 const AdminItemAddModal = ({ open, handleClose }) => {
   const [product, setProduct] = useState({
@@ -14,6 +23,7 @@ const AdminItemAddModal = ({ open, handleClose }) => {
     fuel: 0, // Initialize fuel as 0
     category: "",
     option: "",
+    color: "",
   });
 
   const handleChange = (event) => {
@@ -31,47 +41,31 @@ const AdminItemAddModal = ({ open, handleClose }) => {
     }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // FormData 인스턴스 생성
-    const formData = new FormData();
-    // 폼 데이터에 텍스트 필드 추가
-    formData.append("carId", product.carId);
-    formData.append("carName", product.carName);
-    formData.append("carPrice", product.carPrice);
-    formData.append("speed", product.speed);
-    formData.append("mileage", product.mileage);
-    formData.append("fuel", product.fuel);
-    formData.append("category", product.category);
-    formData.append("option", product.option);
-
-    // 이미지 파일이 있으면 폼 데이터에 파일 추가
-    if (product.img) {
-      console.log("이미지 파일 있음");
-      formData.append("img", product.img);
-      console.log("product", product);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // 이미지 파일의 참조를 상태에 저장
+      setProduct((prevProduct) => ({
+        ...prevProduct,
+        img: file.name,
+      }));
     }
-
-    // 폼 데이터와 함께 API 요청 처리
-    handleAddProduct(formData);
   };
 
-  // 상품 등록
-  const handleAddProduct = async (productToformData) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    const productData = { ...product };
+    formData.append("data", JSON.stringify(productData));
+
+    // 폼 데이터와 함께 API 요청 처리
     try {
-      const response = await addProduct(productToformData);
-      if (response.status === 201) {
-        // 성공적으로 상품이 등록되었을 때의 처리
-        handleClose(); // 모달 닫기
-      } else {
-        // 상품 등록 실패
-        console.log(product);
-        alert("상품 등록에 실패했습니다.");
-      }
+      const response = await addProduct(formData);
+      console.log(response);
+      // ... 상품 등록 성공 또는 실패 처리
     } catch (error) {
-      // 오류 처리
-      console.error(error);
+      // ... 오류 처리
     }
   };
 
@@ -83,84 +77,115 @@ const AdminItemAddModal = ({ open, handleClose }) => {
         left: "50%",
         transform: "translate(-50%, -50%)",
         width: 400,
-        backgroundColor: "white",
-        padding: "20px",
+        bgcolor: "background.paper",
+        boxShadow: 24,
+        p: 4,
+        display: "flex",
+        flexDirection: "column",
+        gap: 2, // 간격
       }}
     >
-      <h2>새로운 상품 등록</h2>
-      <form method="post" enctype="multipart/form-data" onSubmit={handleSubmit}>
-        <TextField
-          label="차량 명칭"
-          name="carName"
-          value={product.carName}
-          onChange={handleChange}
-        />
-        <TextField
-          label="가격(원)"
-          type="number"
-          name="carPrice"
-          value={product.carPrice}
-          onChange={handleChange}
-        />
-        <input
-          type="file"
-          onChange={(event) => {
-            console.log("이벤트 객체", event.target.files[0]);
-            setProduct((prevProduct) => ({
-              ...prevProduct,
-              img: event.target.files[0].name,
-            }));
-          }}
-        />
-        <TextField
-          label="속도"
-          type="number"
-          name="speed"
-          value={product.speed}
-          onChange={handleChange}
-        />
-        <TextField
-          label="주행거리"
-          type="number"
-          name="mileage"
-          value={product.mileage}
-          onChange={handleChange}
-        />
-        <TextField
-          label="연비"
-          type="number"
-          name="fuel"
-          value={product.fuel}
-          onChange={handleChange}
-        />
-        <Select
-          label="카테고리"
-          name="category"
-          value={product.category}
-          onChange={handleChange}
-        >
-          <option value="sedan">sedan</option>
-          <option value="suv">SUV</option>
-          <option value="electric">electric</option>
-        </Select>
-        <Select
-          label="색상"
-          name="option"
-          value={product.option}
-          onChange={handleChange}
-        >
-          <option value="white">white</option>
-          <option value="black">black</option>
-          <option value="gray">gray</option>
-        </Select>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-        >
-          새로운 상품 등록
-        </Button>
+      <Typography variant="h6" component="h2">
+        새로운 상품 등록
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="차량 명칭"
+              name="carName"
+              value={product.carName}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="가격(원)"
+              type="number"
+              name="carPrice"
+              value={product.carPrice}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" component="label">
+              이미지 업로드
+              <input type="file" hidden onChange={handleFileChange} />
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="속도 (km/h)"
+              type="number"
+              name="speed"
+              value={product.speed}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="주행거리 (km)"
+              type="number"
+              name="mileage"
+              value={product.mileage}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel>카테고리</InputLabel>
+              <Select
+                label="카테고리"
+                name="category"
+                value={product.category}
+                onChange={handleChange}
+              >
+                <MenuItem value="sedan">sedan</MenuItem>
+                <MenuItem value="suv">SUV</MenuItem>
+                <MenuItem value="electric">electric</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel>옵션</InputLabel>
+              <Select
+                label="옵션"
+                name="option"
+                value={product.option}
+                onChange={handleChange}
+              >
+                <MenuItem value="light">light</MenuItem>
+                <MenuItem value="signature">signature</MenuItem>
+                <MenuItem value="special">special</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel>색상</InputLabel>
+              <Select
+                label="색상"
+                name="color"
+                value={product.color}
+                onChange={handleChange}
+              >
+                <MenuItem value="white">white</MenuItem>
+                <MenuItem value="black">black</MenuItem>
+                <MenuItem value="gray">gray</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <Button fullWidth type="submit" variant="contained" color="primary">
+              새로운 상품 등록
+            </Button>
+          </Grid>
+        </Grid>
       </form>
     </Box>
   );
