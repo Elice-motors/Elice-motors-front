76d88e551110f693f2a9cf11046528e4b58a83bb
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-11-09 19:51:10 +0900

    feat: 특정 유저의 주문내역 가져오기

--- src/pages/order/OrdersList.js
+++ src/pages/order/OrdersList.js
@@ -1,44 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import OrderHistory from "../../components/order/order-detail/OrderHistory";
 import { Container } from "@mui/material";
+import { getUserOrders } from "../../lib/api";
 
 const OrdersList = () => {
-  const initialOrders = [
-    {
-      orderNumber: 1,
-      userId: "dodo",
-      carImage: "/car1.jpg",
-      carName: "Car 1",
-      option: "라이트",
-      color: "Black",
-      price: 10000000,
-      status: "주문 완료",
-    },
-    {
-      orderNumber: 2,
-      userId: "hihi",
-      carImage: "/car2.jpg",
-      carName: "Car 2",
-      option: "옵션2",
-      color: "White",
-      price: 10000000,
-      status: "배송 완료",
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getUserOrders();
+        if (response.status === 200) {
+          setOrders(response.data.orders);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-  const [orders, setOrders] = useState(initialOrders);
-
-  // 필터 함수를 사용하여 "주문 완료" 상태의 주문만 필터링
-  const completedOrders = orders.filter(
-    (order) => order.status === "주문 완료"
-  );
-
-  // "주문 완료" 주문의 총 결제 금액을 계산
-  const totalAmount = completedOrders.reduce(
-    (total, order) => total + order.price,
-    0
-  );
-
+  const [orders, setOrders] = useState([]);
   const cancelOrder = (orderNumber) => {
     const updatedOrders = orders.filter(
       (order) => order.orderNumber !== orderNumber
@@ -48,7 +28,6 @@ const OrdersList = () => {
 
     alert(`주문 번호 ${orderNumber}가 취소되었습니다.`);
   };
-
   return (
     <div
       style={{
@@ -57,11 +36,7 @@ const OrdersList = () => {
       }}
     >
       <Container maxWidth="sm" sx={{ marginTop: "80px" }}>
-        <OrderHistory
-          orders={orders}
-          totalAmount={totalAmount}
-          cancelOrder={cancelOrder}
-        />
+        <OrderHistory orders={orders} cancelOrder={cancelOrder} />
       </Container>
     </div>
   );
