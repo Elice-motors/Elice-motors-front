65cc9ec11fe36530f770b6a2b2eb0fdeb4b1ac50
min-s9709 <78842816+min-s9709@users.noreply.github.com>
2023-11-10 14:40:44 +0900

    refactor: 주문내역 리팩토링 및 불필요한 코드 삭제

--- src/pages/order/OrdersList.js
+++ src/pages/order/OrdersList.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
-import OrderHistory from "../../components/order/order-detail/OrderHistory";
-import { Container } from "@mui/material";
-import { getUserOrders } from "../../lib/api";
+import { Chip, Container, Typography } from "@mui/material";
+import { deleteUserOrder, getUserOrders } from "../../lib/api";
+import OrderItem from "../../components/order/order-detail/OrderItem";
+
+const textStyle = {
+  fontWeight: "bold",
+  marginBottom: "10px",
+};
 
 const OrdersList = () => {
+  const [orders, setOrders] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -18,7 +25,19 @@ const OrdersList = () => {
     fetchData();
   }, []);
 
-  const [orders, setOrders] = useState([]);
+  const cancelOrder = async (orderNumber) => {
+    try {
+      const response = await deleteUserOrder(orderNumber);
+      if (response.status === 200) {
+        setOrders((prev) =>
+          prev.filter((item) => item.orderNumber !== orderNumber)
+        );
+        alert("주문삭제 완료");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div
       style={{
@@ -27,7 +46,35 @@ const OrdersList = () => {
       }}
     >
       <Container maxWidth="sm" sx={{ marginTop: "80px" }}>
-        <OrderHistory orders={orders} />
+        <Typography variant="h5" style={textStyle}>
+          주문 내역
+        </Typography>
+        {orders?.map((order, index) => (
+          <React.Fragment key={index}>
+            <Typography variant="h6" style={textStyle}>
+              주문번호: {order.orderNumber}
+            </Typography>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography variant="h6" style={textStyle}>
+                배송지: {order.address}
+              </Typography>
+              <Chip
+                label={order.status}
+                sx={{
+                  bgcolor: "primary.main",
+                  fontWeight: "bold",
+                  color: "white",
+                }}
+              />
+            </div>
+            <OrderItem
+              key={index}
+              order={order}
+              isDelivered={false}
+              cancelOrder={cancelOrder}
+            />
+          </React.Fragment>
+        ))}
       </Container>
     </div>
   );
